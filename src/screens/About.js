e62017e67f1985765d0860f28cc24b7be0eb8b34
montgomeryrd <montgomery_rd@yahoo.com>
2021-04-24 23:08:40 -0700

    attempting a bio or sorts in About.js

---
+++ src/screens/About.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <p>
+                For years, I attributed Art as the aperture of my passion, whether it was 
+                drawing, acrylic painting, or playing guitar. It wasn't until I began to 
+                learn web development that helped me process why I felt a passion towards art. 
+                I realized that I enjoyed creating, building functionality, producing beauty, 
+                and making something engaging or stimulating. Cause I also like putting 
+                together new PCs, I enjoy making pizza, and I've even found residential/commercial 
+                painting rewarding for a time. The scope of web development encapsulates the acts 
+                that give fuel to my passion and I hope to develop a career as a software developer.
+            </p>
+
+            <p>
+                I currently lack professional experience, but I hope this portfolio provides 
+                a sense of my abilities. The focus of my portfolio happens to be more front 
+                end development, but I do consider myself a full stack developer with a notable 
+                flair of front end skill and intend to continue to develop my skills in that direction. The theme of 
+                my portfolio is versatility in design. I've taken several popular themes of the 
+                last year and wanted to illustrate 
+            </p>
+        </div>
+    )
+}
+
+export default About;
